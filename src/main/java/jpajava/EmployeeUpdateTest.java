67c3ee2a1db5a8b0ff13b73431f1f792dc2193de
zy_zero <jiyoungjang0307@gmail.com>
2023-11-02 12:19:16 +0900

    employee remove test

--- src/main/java/jpajava/EmployeeUpdateTest.java
+++ src/main/java/jpajava/EmployeeUpdateTest.java
@@ -7,36 +7,37 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-public class EmployeeRemoveTest {
+public class EmployeeUpdateTest {
     public static void main(String[] args) {
 //        Entity Manager를 Persistance 객체의 도움을 받아서 생성해보자
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
 
+        tx.begin();
         try {
-            tx.begin();
             System.out.println("트랜잭션 시작 !!!");
-            System.out.println("select는 즉시 값을 가져옴");
+
             Employee e1 = em.find(Employee.class, "202301");
             System.out.println("DB에서 가져옴!!");
             System.out.println("영속 상태");
 
-            em.remove(e1);
-            System.out.println("removed 상태");
+            System.out.println("수정 전 ==> ");
+            e1.setDeptId(3);
+
+            em.persist(e1);
+            System.out.println("수정 후 ==> ");
 
             System.out.println("====== 커밋 전 ======");
             tx.commit();
-            System.out.println("영속성 컨텍스트의 장점 - 지연 로딩(Lazy Loading)");
             System.out.println("====== 커밋 후 ======");
 
             Employee e2 = em.find(Employee.class, "202301");
-            System.out.println("DB에서 못가져와야 함 (DB는 현재 지워진 상태)");
-            System.out.println("e2 is null ? ==> " + (e2));
-
+            System.out.println("e1 == e2 ? " + (e1 == e2));
         } catch (Exception e) {
             System.out.println(e.getMessage());
             tx.rollback();
+
         } finally {
             em.close();
             emf.close();
